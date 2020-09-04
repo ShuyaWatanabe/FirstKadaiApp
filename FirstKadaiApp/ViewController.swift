cb00eb22cb49f39861e1e9c75e0ba1b4a06f6eba
shuya watanabe <swatanabe990104@gmail.com>
2020-09-04 16:34:33 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Shuya Watanabe on 2020/09/04.
+//  Copyright © 2020 Shuya Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
